d4f68c74cbc3301c9c8589e53c691d061871b7d6
garysenoc <garysenoc@gmail.com>
2022-12-19 10:40:39 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,74 @@
+const express = require('express');
+const cors = require('cors');
+
+const url = require('url');
+const path = require('path');
+
+const app = express();
+app.use(express.json({ limit: '50mb' }));
+
+app.use(cors());
+const { initializeApp } = require('firebase/app');
+const {
+  getStorage,
+  ref,
+  uploadBytes,
+  getDownloadURL,
+} = require('firebase/storage');
+
+const pdfCoApiKey = process.env.PDFCOAPIKEY || 
+  'tetannuz@gmail.com_06afec6ab72c8477278a19818d03fba37fa02057fc86e1234d6904363699f7e442e83b5a';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAKwgJJTj4uktZOZhzYyXwka6370Y_IyYQ',
+  authDomain: 'bionic-19405.firebaseapp.com',
+  projectId: 'bionic-19405',
+  storageBucket: 'bionic-19405.appspot.com',
+  messagingSenderId: '548865965414',
+  appId: '1:548865965414:web:87ca5d98169e511b0a9ca7',
+  measurementId: 'G-KCX1Z76M8D',
+};
+const firebase = initializeApp(firebaseConfig);
+const storage = getStorage(firebase);
+
+const pdfToBionicWithApi = require('./parser/pdfToBionicWithApi');
+const pdfToBionicNoApi = require('./parser/pdfToBionicNoApi');
+
+app.post('/api/bionic', async (req, res) => {
+  try {
+    const files = await pdfToBionicNoApi(req.body.url);
+    const filename = path.posix.basename(url.parse(req.body.url).pathname, ".pdf");
+
+    if (files.error) {
+      const status = typeof files.status !== 'number' ? files.errorCode : files.status;
+      res.status(status).json(files);
+      return;
+    }
+
+    // const pdfRef = ref(storage, `bionic_files/${filename}.pdf`);
+    // const pdfSnapshot = await uploadBytes(pdfRef, files.pdf);
+    // const pdfUrl = await getDownloadURL(pdfSnapshot.ref);
+
+    const htmlRef = ref(storage, `bionic_files/${filename}.html`);
+    const htmlSnapshot = await uploadBytes(htmlRef, files.html);
+    const htmlUrl = await getDownloadURL(htmlSnapshot.ref);
+
+    console.log('Uploaded.');
+
+    res.status(201).json({ htmlUrl });
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: true,
+      message: 'Error occurred while converting or uploading. Please view server logs for more details.'
+    });
+  }
+
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Listening on :${port}`);
+});
+
