1d827c27e44ec7018ba342264151bdbdd52baf5f
Kevin Mallari <kevinmallari62@gmail.com>
2023-01-25 00:51:26 +0800

    remove install puppeteer sh

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.post("/api/bionic", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8081;
 app.listen(port, () => {
   console.log(`Listening on :${port}`);
 });
