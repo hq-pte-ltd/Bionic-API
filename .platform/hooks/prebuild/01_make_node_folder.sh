daa96362a43973588d6d1503dc55ca7a399b027d
Kevin Mallari <kevinmallari62@gmail.com>
2023-01-25 15:48:59 +0800

    update prebuild script

--- .platform/hooks/prebuild/01_make_node_folder.sh
+++ .platform/hooks/prebuild/01_make_node_folder.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 mkdir -p node_modules
--- .platform/hooks/prebuild/02_install_puppeteer.sh
+++ .platform/hooks/prebuild/02_install_puppeteer.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-source /home/ec2-user/.bash_profile
 sudo amazon-linux-extras install epel -y
 sudo yum install -y cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev libcups2 chromium
