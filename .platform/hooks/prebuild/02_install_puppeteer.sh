65759514e92ada0167c15af563fd53b34bb9832d
Kevin Mallari <kevinmallari62@gmail.com>
2023-01-28 01:11:03 +0800

    update prebuild script

--- .platform/hooks/prebuild/02_install_puppeteer.sh
+++ .platform/hooks/prebuild/02_install_puppeteer.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sudo amazon-linux-extras install epel -y
-# sudo yum install -y cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev libcups2 chromium
+sudo amazon-linux-extras install epel -y
+sudo yum install -y cups-libs dbus-glib libXrandr libXcursor libXinerama cairo cairo-gobject pango libnss3-dev libgdk-pixbuf2.0-dev libgtk-3-dev libxss-dev libcups2 chromium
